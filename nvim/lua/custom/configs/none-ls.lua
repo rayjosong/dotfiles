5882f3ba18a2ab09516968a87263951a6382bf13
Raymond Ong <rayjosong@gmail.com>
2025-07-01 08:55:12 +0800

    fix none ls

--- nvim/lua/custom/configs/none-ls.lua
+++ nvim/lua/custom/configs/none-ls.lua
@@ -6,7 +6,6 @@ local opts = {
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.goimports,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint,
   },
   on_attach = function (client, bufnr)
     if client.supports_method("textDocument/formatting") then
